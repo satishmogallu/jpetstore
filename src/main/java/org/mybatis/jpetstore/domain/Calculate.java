d1bc32c4e10f15e97fd1802dd556548ed8c38ba4
Ubuntu <utsav@jenkinsVM.fccfqjldplquxhhqqb35lusond.ix.internal.cloudapp.net>
2016-12-02 12:36:13 +0000

    Changed java file

--- src/main/java/org/mybatis/jpetstore/domain/Calculate.java
+++ src/main/java/org/mybatis/jpetstore/domain/Calculate.java
@@ -11,33 +11,12 @@ public void hello()
 }
 
 }
-//Comment added on date:Fri Dec  2 09:27:04 UTC 2016
-//Comment added on date:Fri Dec  2 09:30:46 UTC 2016
-//Comment added on date:Fri Dec  2 09:31:06 UTC 2016
-//Comment added on date:Fri Dec  2 09:32:20 UTC 2016
-//Comment added on date:Fri Dec  2 09:35:03 UTC 2016
-//Comment added on date:Fri Dec  2 09:43:21 UTC 2016
-//----------------------------------------------------
-//Comment added on date:Fri Dec  2 09:44:14 UTC 2016
-//Author: Andrew Woods, Apoorva Rao
-//Description: Adding coments for documentation
-//Project: JpetStore
-//Tools used: Jenkins, SonarQube, Rundeck
-//----------------------------------------------------
 //----------------------------------------------------
 //Comment added on date:Fri Dec  2 09:45:31 UTC 2016
 //Author: Andrew Woods, Apoorva Rao
 //Description: Adding coments for documentation
 //Project: JpetStore
 //Tools used: Jenkins, SonarQube, Rundeck
-//----------------------------------------------------
-//----------------------------------------------------
-//Comment added on date:Fri Dec  2 09:46:51 UTC 2016
-//Author: Andrew Woods, Apoorva Rao
-//Description: Adding coments for documentation
-//Project: JpetStore
-//Tools used: Jenkins, SonarQube, Rundeck
-//----------------------------------------------------
 //----------------------------------------------------
 //Comment added on date:Fri Dec  2 09:55:14 UTC 2016
 //Author: Andrew Woods, Apoorva Rao
