0c00990d9597fc2a1f00902350e6f93dc5e7d0be
Ubuntu <ManiPrasanna@Devopswin1.vbpeb4h255tupifkvvfwma2fcd.bx.internal.cloudapp.net>
2016-12-02 09:30:47 +0000

    Fri Dec  2 09:30:46 UTC 2016

---
+++ src/main/java/org/mybatis/jpetstore/domain/Calculate.java
@@ -0,0 +1,29 @@
+package org.mybatis.jpetstore.domain;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class ItemCalculate implements Serializable {
+
+  private String itemId;
+  private String productId;
+  private BigDecimal listPrice;
+  private BigDecimal unitCost;
+  private int supplierId;
+  private String status;
+  private String attribute1;
+  private String attribute2;
+  private String attribute3;
+  private String attribute4;
+  private String attribute5;
+  private Product product;
+  private int quantity;
+
+  public String CalculateTotal(int quantity, BigDecimal unitCost) {
+    return quantity*unitCost;
+  }
+
+
+}
+#Comment added on date:Fri Dec  2 09:27:04 UTC 2016
+#Comment added on date:Fri Dec  2 09:30:46 UTC 2016
