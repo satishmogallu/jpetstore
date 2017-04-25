5d9c854c7d979100a4edd21485b24ed6c8029aa5
Ubuntu <devopsadm@POCMachine.a0so2nyxsltuvjbpknwjqqomqf.ix.internal.cloudapp.net>
2017-04-25 11:00:09 +0000

    Tue Apr 25 11:00:09 UTC 2017/src/main/java/org/mybatis/jpetstore/domain/

--- src/main/java/org/mybatis/jpetstore/domain/Calculate.java
+++ src/main/java/org/mybatis/jpetstore/domain/Calculate.java
@@ -613,3 +613,10 @@ System.out.println("Fri Dec  2 12:52:58 UTC 2016");
 //Project: JpetStore
 //Tools used: Jenkins, SonarQube, Rundeck
 //----------------------------------------------------
+//----------------------------------------------------
+//Comment added on date:Tue Apr 25 11:00:09 UTC 2017
+//Author: Andrew Woods, Apoorva Rao
+//Description: Adding coments for documentation
+//Project: JpetStore
+//Tools used: Jenkins, SonarQube, Rundeck
+//----------------------------------------------------
